913500f7c708cc0d8c5c9ae0728ea3403196fa5a
cyccheng <cyccheng@hotmail.com>
2018-05-25 18:41:31 +0800

    25 May 2018 - 7pm - Cyc

--- app/src/main/java/com/mobapp/checklistapp/TemplateActivity.java
+++ app/src/main/java/com/mobapp/checklistapp/TemplateActivity.java
@@ -3,8 +3,14 @@ package com.mobapp.checklistapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mobapp.checklistapp.util.MobappApplicationState;
+
+import java.util.ArrayList;
 
 /**
  * Created by sherynn on 10/04/2018.
@@ -13,6 +19,8 @@ import com.mobapp.checklistapp.util.MobappApplicationState;
 public class TemplateActivity extends MobappActivity {
 
     private ListView listQuestion;
+    String showQuest;
+    DatabaseReference ref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,19 +30,65 @@ public class TemplateActivity extends MobappActivity {
         super.setNavAddButtonHidden(true);
         super.setNavTitle(getString(R.string.TEMPLATE_NAV_TITLE));
 
-        initUI();
+        ref = FirebaseDatabase.getInstance().getReference("Question");
+
+
+        //initUI();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //String zzz = dataSnapshot.getKey();
+                //String zzz = dataSnapshot.getValue(String.class);
+                //Log.d(TAG, "Value is: " + value);
+                //LoginVO userName = new LoginVO();
+
+                initUI(dataSnapshot);
+                //userName.setUserID(tex.getUser(user));
+                //System.out.println(tex);
+                //System.out.println(zzz.user);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
     }
 
     // ============================================================================================
     // Private Methods
     // ============================================================================================
 
-    private void initUI() {
+    private void initUI(DataSnapshot dataSnapshot) {
         listQuestion = (ListView) findViewById(R.id.listQuestion);
+        int i = 1;
+
+        final ArrayList<String> mylist = new ArrayList<String>();
+        for(DataSnapshot ds: dataSnapshot.getChildren()){
+
+            mylist.add(ds.getValue(String.class));
+        }
+
+        String[] arrTestQuest = new String[mylist.size()];
+        arrTestQuest = mylist.toArray(arrTestQuest);
+
+
+        //showQuest = dataSnapshot.getValue(String.class);
 
         final String[] arrQuestion = MobappApplicationState.getInstance().getCurrentActiveContext().getResources().getStringArray(R.array.GH2_1);
 //        ArrayAdapter adapter = new ArrayAdapter<String>(MobappApplicationState.getInstance().getCurrentActiveContext(), R.layout.activity_listitemrow_question, arrQuestion);
-        QuestionListViewAdapter adapter = new QuestionListViewAdapter(MobappApplicationState.getInstance().getCurrentActivity(), arrQuestion);
+        QuestionListViewAdapter adapter = new QuestionListViewAdapter(MobappApplicationState.getInstance().getCurrentActivity(), arrTestQuest);
         listQuestion.setAdapter(adapter);
 
         View header = (View) getLayoutInflater().inflate(R.layout.view_template_header, null);
--- app/src/main/java/com/mobapp/checklistapp/TemplateView.java
+++ app/src/main/java/com/mobapp/checklistapp/TemplateView.java
@@ -2,6 +2,9 @@ package com.mobapp.checklistapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +12,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mobapp.checklistapp.util.MobappApplicationState;
 
 /**
@@ -18,8 +25,39 @@ import com.mobapp.checklistapp.util.MobappApplicationState;
 
 public class TemplateView extends FrameLayout {
 
+
     private Context parentContext;
     private ListView listTemplate;
+    String showTemplate_data;
+    private FirebaseDatabase mFdb;
+    private DatabaseReference ref;
+
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        ref = FirebaseDatabase.getInstance().getReference("userID");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //String zzz = dataSnapshot.getKey();
+                //String zzz = dataSnapshot.getValue(String.class);
+                //Log.d(TAG, "Value is: " + value);
+                //LoginVO userName = new LoginVO();
+                showTemplate_data = dataSnapshot.getValue(String.class);
+                //userName.setUserID(tex.getUser(user));
+                //System.out.println(tex);
+                //System.out.println(zzz.user);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 
     public TemplateView(Context context) {
         super(context);
@@ -40,18 +78,24 @@ public class TemplateView extends FrameLayout {
         initData();
     }
 
+
+
     // ============================================================================================
     // Private Methods
     // ============================================================================================
 
+    DatabaseReference mDatabase;
+
+
     private void initData() {
         //Load payment entry xml
+
         LayoutInflater layoutInflater = (LayoutInflater) parentContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         layoutInflater.inflate(R.layout.template_view, this);
 
         listTemplate = (ListView) findViewById(R.id.listTemplate);
-
-        final String[] arrTemplate = {"Template 1","Template 2","Template 3"};
+        showTemplate_data = "Template 33";
+        final String[] arrTemplate = {"Template 1","Template 2", showTemplate_data};
         ArrayAdapter adapter = new ArrayAdapter<String>(MobappApplicationState.getInstance().getCurrentActiveContext(), R.layout.activity_listitemrow, arrTemplate);
         listTemplate.setAdapter(adapter);
 
@@ -64,4 +108,7 @@ public class TemplateView extends FrameLayout {
             }
         });
     }
+
+
+
 }
